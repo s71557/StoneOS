3d110a7030f5332cef3622222e79c3d177cf8104
Stone <74167978+s71557@users.noreply.github.com>
2023-08-13 12:43:35 +0800

    Update Renames.sh

--- Scripts/Renames.sh
+++ Scripts/Renames.sh
@@ -5,7 +5,7 @@ for var in $OpenWrt_TYPE ; do
   for file in $(find ./ -type f -iname "*$var*.*" ! -iname "*.txt") ; do
     export ext=$(basename "$file" | cut -d '.' -f 2-3)
     export name=$(basename "$file" | cut -d '.' -f 1 | grep -io "\($var\).*")
-    export new_file="StoneOS"_"$OpenWrt_SOURCE"_"$name"_"$OpenWrt_DATE"."$ext"
+    export new_file="StoneOS"_"$OpenWrt_SOURCE"_"linux.6.1"_"$name"_"$OpenWrt_DATE"."$ext"
     mv -f "$file" "$new_file"
   done
 done
