ba7fc861d22b77cfa8a95a7e87a7ba0a85f8df86
Stone <74167978+s71557@users.noreply.github.com>
2023-08-15 19:43:38 +0800

    更新 Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Alist
-svn export https://github.com/sbwml/luci-app-alist/trunk/luci-app-alist package/luci-app-alist
-svn export https://github.com/sbwml/luci-app-alist/trunk/alist package/alist
+# svn export https://github.com/sbwml/luci-app-alist/trunk/luci-app-alist package/luci-app-alist
+# svn export https://github.com/sbwml/luci-app-alist/trunk/alist package/alist
 
