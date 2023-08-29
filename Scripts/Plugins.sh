66ca747a1a3f89f13cabecfc516ac15c1daa2ec8
Stone <74167978+s71557@users.noreply.github.com>
2023-08-30 07:53:08 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -4,3 +4,4 @@
 # svn export https://github.com/sbwml/luci-app-alist/trunk/luci-app-alist package/luci-app-alist
 # svn export https://github.com/sbwml/luci-app-alist/trunk/alist package/alist
 
+curl -o ./feeds/packages/multimedia/aliyundrive-webdav/Makefile https://raw.githubusercontent.com/Jason6111/OpenWrt_Personal/main/other/aliyun/Makefile
