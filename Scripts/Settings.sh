31048ac771c229afe51e5d7d5535d043b2a41338
Stone <74167978+s71557@users.noreply.github.com>
2023-09-15 12:54:06 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -6,18 +6,20 @@ rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|argon\).*")
 #修改默认主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon 
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
+
 #修改默认IP地址
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OpenWrt_IP/g" ./package/base-files/files/bin/config_generate
+
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='$OpenWrt_NAME'/g" ./package/base-files/files/bin/config_generate
+
 #修改默认时区
 sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
-#修改R2S leds配置
-#sed -i '/r2s/{n;n;n;i \    ucidef_set_led_netdev "wlan" "WLAN" "green:wlan" "wlan0"
-#}' target/linux/rockchip/armv8/base-files/etc/board.d/01_leds
+
 #编译6.1内核
 sed -i 's/5.15/6.1/g' ./target/linux/rockchip/Makefile
+
 #加入编译作者信息
 new_name="StoneOS $(date +"%Y-%m-%d")"
 sed -i "s/OpenWrt /$new_name/g" package/lean/default-settings/files/zzz-default-settings
